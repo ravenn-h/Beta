46135f2837d594e849d45b33fd1fae36a9b2d191
hhhisoka <enochkouame123@gmail.com>
2025-09-25 20:44:00 +0000

    update

---
+++ error.js
@@ -0,0 +1,37 @@
+const colors = require('colors')
+const chalk = require('chalk')
+const moment = require('moment-timezone')
+
+process.on('uncaughtException', err => {
+   const date = moment(Date.now()).format('DD/MM/YY HH:mm:ss')
+   if (err?.code === 'ENOMEM') {
+      console.error(chalk.black(chalk.bgRed(` Exception `)), chalk.black(chalk.bgBlue(` ${date} `)), ':', colors.gray('Out of memory error detected. Cleaning up resources'))
+   } else {
+      console.error(chalk.black(chalk.bgRed(` Exception `)), chalk.black(chalk.bgBlue(` ${date} `)), ':', colors.gray(err))
+   }
+   setTimeout(() => process.exit(1), 100)
+})
+
+const unhandledRejections = new Map()
+
+process.on('unhandledRejection', (reason, promise) => {
+   unhandledRejections.set(promise, reason)
+   if (reason?.message?.includes('Timed') || reason?.message?.includes('SessionError') || reason?.message?.includes('ENOENT')) return
+   const date = moment(Date.now()).format('DD/MM/YY HH:mm:ss')
+   console.error(chalk.black(chalk.bgRed(` Rejection `)), chalk.black(chalk.bgBlue(` ${date} `)), ':', colors.gray(reason))
+   setTimeout(() => process.exit(1), 100)
+})
+
+process.on('rejectionHandled', promise => {
+   unhandledRejections.delete(promise)
+})
+
+// process.on('exit', () => { })
+
+process.on('warning', (warning) => {
+   if (warning?.name === 'MaxListenersExceededWarning') {
+      console.warn(colors.yellow('Potential memory leak detected.'))
+   }
+})
+
+// process.stderr.write = () => {}  // Disable all stderr output (Extra Clean 💀) not recommended
---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config(), require('rootpath')(), console.clear()
+const { spawn: spawn } = require('child_process'), path = require('path'), CFonts = require('cfonts'), chalk = require('chalk')
+
+function start() {
+   let args = [path.join(__dirname, 'client.js'), ...process.argv.slice(2)]
+   let p = spawn(process.argv[0], args, {
+      stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+   }).on('message', data => {
+      if (data == 'reset') {
+         console.log('Restarting...')
+         p.kill()
+         delete p
+      }
+   }).on('exit', code => {
+      console.error('Exited with code:', code)
+      start()
+   })
+}
+
+const major = parseInt(process.versions.node.split('.')[0], 10)
+if (major < 20) {
+   console.error(
+      `\n❌ This script requires Node.js 20+ to run reliably.\n` +
+      `   You are using Node.js ${process.versions.node}.\n` +
+      `   Please upgrade to Node.js 20+ to proceed.\n`
+   );
+   process.exit(1)
+}
+
+CFonts.say('NYXEL', {
+   font: 'tiny',
+   align: 'center',
+   colors: ['system']
+}), CFonts.say('Github : https://github.com/NyxelTech, {
+   colors: ['system'],
+   font: 'console',
+   align: 'center'
+}), start()
